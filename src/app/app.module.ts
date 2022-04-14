92c6ac750400560ccb9904100f174d600e97e815
shimonlebo <58909312+shimonlebo@users.noreply.github.com>
2022-04-14 08:53:47 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ButtonComponent } from './shared/button/button.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ButtonComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
